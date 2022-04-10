f8626401a8e16b4cd10935bb0fe03b57da4b5e4a
LGRN424 <LGRN424@GMAIL.COM>
2022-04-10 07:02:28 +0300

    Pay Comparison Program

---
+++ ComparisonMathAssign/ComparisonMathAssign/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComparisonMathAssign
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //User input//
+            Console.WriteLine("Enter your favorite number: ");
+            int favNum = Convert.ToInt32(Console.ReadLine());
+
+            //Mulitply input//
+            int multiNum = favNum * 50;
+            Console.WriteLine("Your favorite number times 50 is: " + multiNum);
+          
+
+            //User input plus 25//
+            Console.WriteLine("Enter your age: ");
+            int ageNum = Convert.ToInt32(Console.ReadLine());
+            int agePlus = ageNum + 25;
+            Console.WriteLine("You age in 25 years: " + agePlus);
+
+            //User input plus 25//
+            Console.WriteLine("Enter the square footage of your house: ");
+            double val = Convert.ToDouble(Console.ReadLine());
+            double footHouse = val / 12.5;
+            Console.WriteLine("The floors in your house are:  " + footHouse);
+
+            //User input greater than 50 if true or false//
+            Console.WriteLine("Enter the miles of your last car trip: ");
+            int carMiles = Convert.ToInt32(Console.ReadLine());
+            int moreMiles = 50;
+            bool isMore = carMiles > moreMiles;
+            Console.WriteLine(isMore);
+
+            //User input division and remainder//
+            Console.WriteLine("Enter the number of socks you own: ");
+            int socksNum = Convert.ToInt32(Console.ReadLine());
+            int socksLeftOver = socksNum % 7;
+            Console.WriteLine("The number of left over socks is: " + socksLeftOver);
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
---
+++ IncomeComparison/IncomeComparison/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IncomeComparison
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Anonymous Income Comparison Program");
+
+            //Person 1 pay rate and hours per week//
+            Console.WriteLine("Person 1");
+            Console.WriteLine("Hourly Rate?");
+            int rate1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Hours worrked per week");
+            int hours1 = Convert.ToInt32(Console.ReadLine());
+
+            //Person 2 pay rate and hours per week//
+            Console.WriteLine("Person 2");
+            Console.WriteLine("Hourly Rate?");
+            int rate2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Hours worrked per week");
+            int hours2 = Convert.ToInt32(Console.ReadLine());
+
+            //Annual salary of each person//
+            int salary1 = (rate1 + hours1) * 52;
+            int salary2 = (rate2 + hours2) * 52;
+
+            Console.WriteLine("Annual Salary of Person 1:" );
+            Console.WriteLine(salary1);
+
+            Console.WriteLine("Annual Salary of Person 2:");
+            Console.WriteLine(salary2);
+
+            //Comparison of salaries//
+            bool moreMoney = salary1 > salary2;
+            Console.WriteLine(moreMoney);
+            Console.ReadLine();
+
+
+        }
+    }
+}
