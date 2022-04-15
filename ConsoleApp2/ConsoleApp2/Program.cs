bf3a80749ed62a0b822d3da2bd1b443e9088dcc8
LGRN424 <LGRN424@GMAIL.COM>
2022-04-16 00:33:04 +0300

    LoopwithSwitch

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            bool hasTreats = false;
+
+            
+            while (!hasTreats)
+            {
+                Console.WriteLine("How many treats do you have?");
+                int numbTreats = Convert.ToInt32(Console.ReadLine());
+
+                if (numbTreats <= 4)
+                {
+                    Console.WriteLine("The dog will be disappointed");
+                }
+                else 
+                {
+                    Console.WriteLine("Your are the dog's favorite person until someone else brings more treats!");
+                }
+                break;
+
+                
+
+            }
+            Console.ReadLine();
+        }
+
+    }
+}
+ 
---
+++ Loops/Loops/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Loops
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Guess a number:");
+            int numb =  Convert.ToInt32(Console.ReadLine());
+            bool isGuessed = numb == 12;
+
+            while (!isGuessed)
+            {
+                switch (numb)
+                {
+                    case 62:
+                        Console.WriteLine("You guessed 62.  Try again!");
+                        Console.WriteLine("Guess a number:");
+                        numb = Convert.ToInt32(Console.ReadLine());
+                        break;
+
+                    case 29:
+                        Console.WriteLine("You guessed 29.  Try again!");
+                        Console.WriteLine("Guess a number:");
+                        numb = Convert.ToInt32(Console.ReadLine());
+                        break;
+
+                    case 12:
+                        Console.WriteLine("You guessed 12.  That is correct!");
+                        isGuessed = true;
+                        break;
+
+                    default:
+                        Console.WriteLine("You are wrong.");
+                        Console.WriteLine("Guess a number:");
+                        numb = Convert.ToInt32(Console.ReadLine());
+                        break;
+
+
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
