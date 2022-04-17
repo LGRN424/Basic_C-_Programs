385804dab8692e12bac7f6ceb9f8beb598284604
LGRN424 <LGRN424@GMAIL.COM>
2022-04-17 07:45:21 +0300

    LoopsandTreatsUpdate

--- Loopsandtreats/Loopsandtreats/Program.cs
+++ Loopsandtreats/Loopsandtreats/Program.cs
@@ -10,9 +10,9 @@ namespace Loops
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("My dog wants to know how many dog treats do you have today?");
+            Console.WriteLine("My dog wants to know how many dog treats do you have today. Enter number of treats:");
             int numb = Convert.ToInt32(Console.ReadLine());
-            bool isGuessed = numb == 8;
+            bool isGuessed = numb == 8; 
 
             while (!isGuessed)
             {
@@ -20,30 +20,36 @@ namespace Loops
                 {
                     case 0:
                     case 1:
-                        Console.WriteLine("You did not bring enough treats.  Bring more next time!!");
+                        Console.WriteLine("You did not bring enough treats!");
+                        Console.WriteLine("How many treats will you bring next time?");
                         numb = Convert.ToInt32(Console.ReadLine());
                         break;
 
                     case 2:
                     case 3:
                         Console.WriteLine("You still did not bring enough treats but at least you brought something!");
+                        Console.WriteLine("How many treats will you bring next time?");
                         numb = Convert.ToInt32(Console.ReadLine());
                         break;
 
                     case 4:
+                    case 5:
 
                         Console.WriteLine("You brought enough treats.  Thank you!");
-                        isGuessed = true;
+                        Console.WriteLine("How many treats will you bring next time?");
+                        numb = Convert.ToInt32(Console.ReadLine()); 
                         break;
 
-                    case 5:
+             
                     case 6:
+                    case 7:
 
                         Console.WriteLine("You brought extra treats! You are nice.");
+                        Console.WriteLine("How many treats will you bring next time?");
                         numb = Convert.ToInt32(Console.ReadLine());
                         break;
 
-                    case 7:
+                    
                     case 8:
                         Console.WriteLine("You are my favorite person!");
                         isGuessed = true;
@@ -51,6 +57,7 @@ namespace Loops
 
                     default:
                         Console.WriteLine("You brought too many treats!");
+                        Console.WriteLine("How many treats will you bring next time?");
                         numb = Convert.ToInt32(Console.ReadLine());
                         break;
 
